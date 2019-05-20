a968df31f8104850b261c7afc06485674143d983
Le Minh Quang <quanglm1998@gmail.com>
2019-05-20 15:40:57 +0700

    add lru cache

---
+++ src/cache.h
@@ -0,0 +1,134 @@
+#ifndef CACHE_H
+#define CACHE_H
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename X, typename Y>
+struct Node {
+    X key;
+    Y value;
+    Node *prev, *next;
+    Node(X k, Y v): key(k), value(v), prev(NULL), next(NULL) {}
+};
+
+template<typename X, typename Y>
+struct DoublyLinkedList {
+  Node<X, Y> *front, *rear;
+  
+  bool isEmpty() {
+      return rear == NULL;
+  }
+
+  DoublyLinkedList(): front(NULL), rear(NULL) {}
+  
+  Node<X, Y>* add_page_to_head(X key, Y value) {
+      Node<X, Y> *page = new Node<X, Y>(key, value);
+      if(!front && !rear) {
+          front = rear = page;
+      }
+      else {
+          page->next = front;
+          front->prev = page;
+          front = page;
+      }
+      return page;
+  }
+
+  void move_page_to_head(Node<X, Y> *page) {
+      if(page==front) {
+          return;
+      }
+      if(page == rear) {
+          rear = rear->prev;
+          rear->next = NULL;
+      }
+      else {
+          page->prev->next = page->next;
+          page->next->prev = page->prev;
+      }
+
+      page->next = front;
+      page->prev = NULL;
+      front->prev = page;
+      front = page;
+  }
+
+  void remove_rear_page() {
+      if(isEmpty()) {
+          return;
+      }
+      if(front == rear) {
+          delete rear;
+          front = rear = NULL;
+      }
+      else {
+          Node<X, Y> *temp = rear;
+          rear = rear->prev;
+          rear->next = NULL;
+          delete temp;
+      }
+  }
+  Node<X, Y>* get_rear_page() {
+      return rear;
+  }
+};
+
+template<typename X, typename Y>
+struct LRUCache{
+  int capacity, size;
+  DoublyLinkedList<X, Y> *pageList;
+  map<X, Node<X, Y>*> pageMap;
+
+  public:
+    LRUCache(int capacity) {
+      this->capacity = capacity;
+      size = 0;
+        pageList = new DoublyLinkedList<X, Y>();
+        pageMap = map<X, Node<X, Y>*>();
+    }
+
+    Y* get(X key) {
+        if(pageMap.find(key)==pageMap.end()) {
+          return NULL;
+        }
+        Y* val = &(pageMap[key]->value);
+
+        // move the page to front
+        pageList->move_page_to_head(pageMap[key]);
+        return val;
+    }
+
+    void set(X key, Y value) {
+      if(pageMap.find(key)!=pageMap.end()) {
+          // if key already present, update value and move page to head
+          pageMap[key]->value = value;
+          pageList->move_page_to_head(pageMap[key]);
+          return;
+      }
+
+        if(size == capacity) {
+          // remove rear page
+          X k = pageList->get_rear_page()->key;
+          pageMap.erase(k);
+          pageList->remove_rear_page();
+          size--;
+        }
+
+        // add new page to head to Queue
+        Node<X, Y> *page = pageList->add_page_to_head(key, value);
+        size++;
+        pageMap[key] = page;
+    }
+
+    ~LRUCache() {
+        for (auto u : pageMap) {
+            delete(u.second);
+        }
+      delete pageList;
+    }
+};
+
+
+#endif CACHE_H
